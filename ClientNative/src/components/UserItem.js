13cdf5a08bffcabfe43e92092b28415efea64f5b
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2023-02-24 10:44:42 +0700

    c39 create style

--- ClientNative/src/components/UserItem.js
+++ ClientNative/src/components/UserItem.js
@@ -4,6 +4,8 @@ import { faTrashCan, faPen } from '@fortawesome/free-solid-svg-icons'
 import { Button, Modal } from 'react-bootstrap'
 import { useDispatch } from "react-redux"
 
+import { View, Text, TouchableOpacity } from "react-native"
+
 export default function UserItem(props) {
 
     const dispatch = useDispatch()
@@ -73,9 +75,9 @@ export default function UserItem(props) {
 
     return (
         <Fragment>
-            <tr>
-                <td>{props.no}</td>
-                <td>
+            <View style={styles.container}>
+                <Text>{props.no}</Text>
+                <Text>
                     {user.isEdit ?
                         <input
                             type="teks"
@@ -87,9 +89,9 @@ export default function UserItem(props) {
                         :
                         user.name
                     }
-                </td>
+                </Text>
 
-                <td>
+                <Text>
                     {user.isEdit ?
                         <input
                             type="teks"
@@ -101,57 +103,57 @@ export default function UserItem(props) {
                         :
                         user.phone
                     }
-                </td>
+                </Text>
 
-                {props.data.sent ?
-                    user.isEdit ?
-                        <td>
-                            <button type="button"
-                                className="btn btn-primary"
-                                onClick={saveEdit}>
-                                <i className="fa-solid fa-floppy-disk"></i>
+                    {props.data.sent ?
+                        user.isEdit ?
+                            <Text>
+                                <TouchableOpacity
+                                    className="btn btn-primary"
+                                    onPress={saveEdit}>
+                                    <i className="fa-solid fa-floppy-disk"></i>
+                                    &nbsp;
+                                    save
+                                </TouchableOpacity>
                                 &nbsp;
-                                save
-                            </button>
-                            &nbsp;
-                            <button type="button"
-                                className="btn btn-warning"
-                                onClick={handleCancel}
-                                style={{ color: "white" }}>
-                                <i className="fa-solid fa-ban"></i>
+                                <TouchableOpacity
+                                    className="btn btn-warning"
+                                    onPress={handleCancel}
+                                    style={{ color: "white" }}>
+                                    <i className="fa-solid fa-ban"></i>
+                                    &nbsp;
+                                    cancel
+                                </TouchableOpacity>
+                            </Text>
+                            :
+                            <Text>
+                                <TouchableOpacity
+                                    className="btn btn-success"
+                                    onPress={handleEdit}>
+                                    <FontAwesomeIcon icon={faPen} />
+                                    &nbsp;
+                                    edit
+                                </TouchableOpacity>
                                 &nbsp;
-                                cancel
-                            </button>
-                        </td>
+                                <TouchableOpacity
+                                    className="btn btn-danger"
+                                    onPress={() => handleModalShowHide()}>
+                                    <FontAwesomeIcon icon={faTrashCan} />
+                                    &nbsp;
+                                    delete
+                                </TouchableOpacity>
+                            </Text>
                         :
-                        <td>
-                            <button type="button"
-                                className="btn btn-success"
-                                onClick={handleEdit}>
-                                <FontAwesomeIcon icon={faPen} />
-                                &nbsp;
-                                edit
-                            </button>
-                            &nbsp;
-                            <button type="button"
-                                className="btn btn-danger"
-                                onClick={() => handleModalShowHide()}>
-                                <FontAwesomeIcon icon={faTrashCan} />
-                                &nbsp;
-                                delete
-                            </button>
-                        </td>
-                    :
-                    <td>
-                        <button type="button"
-                            className="btn btn-warning"
-                            onClick={props.resend}
-                            style={{ color: "white" }}>
-                            resend
-                        </button>
-                    </td>
-                }
-            </tr>
+                        <Text>
+                            <TouchableOpacity
+                                className="btn btn-warning"
+                                onPress={props.resend}
+                                style={{ color: "white" }}>
+                                resend
+                            </TouchableOpacity>
+                        </Text>
+                    }
+            </View>
 
             <Modal show={user.showHide}>
                 <Modal.Header >
@@ -159,10 +161,10 @@ export default function UserItem(props) {
                 </Modal.Header>
                 <Modal.Body>Are you sure, you want delete <b>{props.data.name}</b></Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={cancelHandleModalShowHide}>
+                    <Button variant="secondary" onPress={cancelHandleModalShowHide}>
                         No
                     </Button>
-                    <Button variant="primary" onClick={props.remove}>
+                    <Button variant="primary" onPress={props.remove}>
                         Yes
                     </Button>
                 </Modal.Footer>
@@ -171,3 +173,34 @@ export default function UserItem(props) {
         </Fragment>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: 40,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    remove: {
+        height: 40,
+        width: 75,
+        backgroundColor: "red",
+        borderStyle: "solid",
+        borderColor: "white",
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    resend: {
+        height: 40,
+        width: 75,
+        backgroundColor: "yellow",
+        borderStyle: "solid",
+        borderColor: "white",
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    LabelButton: {
+        color: "fffffff",
+    },
+});
