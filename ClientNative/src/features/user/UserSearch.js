cf13e905db24f4c21f3ca1a799f136b543a9ce30
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2023-03-08 03:14:03 +0700

    resend done

--- ClientNative/src/features/user/UserSearch.js
+++ ClientNative/src/features/user/UserSearch.js
@@ -65,7 +65,6 @@ const styles = StyleSheet.create({
     container: {
         paddingHorizontal: windowWidth * 0.06,
         marginVertical: windowHeight * 0.01,
-
     },
     title: {
         fontSize: 32,
